fb5eac133cb801a245ae87f95af6bcaf4dfacb05
labforweb <aleksandernikollibusiness@gmail.com>
2025-08-06 20:27:47 +0200

    ottimo  commit

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,30 @@
-import { CanActivateFn } from '@angular/router';
+// src/app/guards/auth.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.authService.isLoggedIn$.pipe(
+      take(1), // Prende solo l'ultimo valore e poi completa l'Observable
+      map(isLoggedIn => {
+        if (isLoggedIn) {
+          return true; // L'utente è loggato, permette l'accesso
+        } else {
+          // L'utente non è loggato, reindirizza alla pagina di login
+          return this.router.createUrlTree(['/login']);
+        }
+      })
+    );
+  }
+}
