6cc68414c1c01cf229f15bbbfd7a509d994f0d82
labforweb <aleksandernikollibusiness@gmail.com>
2025-08-20 17:23:21 +0200

    Fix: Risolto errore NG0908 con NoopNgZone

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NgZone } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
+import { ɵNoopNgZone } from '@angular/core'; // Importa NoopNgZone
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,10 @@ import { AppComponent } from './app.component';
   imports: [
     AppModule,
     ServerModule,
+  ],
+  providers: [
+    // Aggiungendo questo provider, risolvi l'errore NG0908
+    { provide: NgZone, useClass: ɵNoopNgZone }
   ],
   bootstrap: [AppComponent],
 })
