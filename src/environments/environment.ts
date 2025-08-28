745bb3b87f398a495475e7b652880f4c6b70fe03
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-08-28 13:51:06 +0200

    aggiornamenti

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  // Modifica la porta da 3000 a 8080 e aggiungi /api
-  apiUrl: 'http://localhost:8080/b-api' 
+  apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/b-api'
+  // apiUrl: 'http://localhost:8080/b-api' 
 };
