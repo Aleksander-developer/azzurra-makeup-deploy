e0c5c9870813622a7467868827c0921e7157d723
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-08-28 15:33:39 +0200

    Fix: Allinea apiUrl del frontend con il backend (da /b-api a /api)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/b-api'
+  apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/api'
 };
