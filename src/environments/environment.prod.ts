ac3f176cf3864c54afbad5305111a0cabdf68a74
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-08-28 11:26:20 +0200

    Fix: Sincronizzazione frontend-backend e risoluzione favicon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
+  apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/b-api'
   // apiUrl: 'http://localhost:8080/b-api'
-  apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/b-api' // <-- AGGIORNATO CON L'URL DEL TUO BACKEND IN PROD
+  
 };
 
