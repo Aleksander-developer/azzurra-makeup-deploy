87e15fdfc64745b9daf9ab6bf84161535d8edb86
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-08-30 13:39:27 +0200

    aggiornamenti

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/api'
+  apiUrl: 'https://azzurra-makeup-deploy-1046780610179.europe-west1.run.app/b-api/'
 };
