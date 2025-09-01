9761e9d1ed0a3d51edbe2a666916ffc98856edbd
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-09-01 21:27:09 +0200

    variabili sul frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   apiUrl: '/b-api' // <-- niente URL assoluto, solo il prefisso gestito dal proxy
   // apiUrl: 'https://azzurra-makeup-deploy-1046780610179.europe-west1.run.app/b-api', // centralizzato tutto dietro il proxy
   // apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/api', // NON diretto al BE
-  // apiUrl: 'http://localhost:8080/api', // solo per test locale
+  // apiUrl: 'http://localhost:8080/api', // solo per test locale.
 };
