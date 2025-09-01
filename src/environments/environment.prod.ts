f036778ddaba24e1729810f172096c7ad6639e00
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-09-01 17:13:19 +0200

    Allineamento finale frontend per deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://azzurra-makeup-deploy-1046780610179.europe-west1.run.app/b-api/'
+  apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/api',
+  // apiUrl: 'http://localhost:8080/api',
 };
