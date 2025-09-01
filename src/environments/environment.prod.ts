db5682118b3ba65e6ae34a72bfa9a5c04d2f7f7e
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-09-01 18:11:24 +0200

    centralizzato tutto dietro il proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/api',
-  // apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://azzurra-makeup-deploy-1046780610179.europe-west1.run.app/b-api', // centralizzato tutto dietro il proxy
+  // apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/api', // NON diretto al BE
+  // apiUrl: 'http://localhost:8080/api', // solo per test locale
 };
