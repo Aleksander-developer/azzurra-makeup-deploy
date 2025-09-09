d454eaec74e9cdc5a15900902df43d80f672c34a
Aleksander Nikolli <aleksandernikollibusiness@gmail.com>
2025-09-09 19:56:09 +0200

    aggionamenti

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://azzurra-makeup-be-377666630750.europe-west1.run.app/api' // backend locale per test SSR
-  // apiUrl: 'https://azzurra-makeup-be-1046780610179.europe-west1.run.app/api'
+  // apiUrl: 'http://localhost:8080/api' // per simulare il backend in locale
 };
+
